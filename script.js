6de8fe885fd439b26e1de591635766afce87a38a
Ali Nawab <alinawab9830@gmail.com>
2023-05-04 18:27:57 -0500

    Adding comments and fixing small bugs

--- script.js
+++ script.js
@@ -44,12 +44,14 @@ function generatePassword() {
     if (specialChars) {
       charset += passwordOptions.character;
     }
+    //Searches the choices to see if the user atleasr selceted one of the criteria
     if(charset.search(/[a-z]/) == -1 && charset.search(/[A-Z]/) == -1 && charset.search(/[0-9]/) == -1){
       alert("Please select at least one password criteria.");
       return null;
     }
 
   var password = "";
+  //Randomizing the password
   for (var i = 0; i < length; i++) {
     var randomChar = charset.charAt(Math.floor(Math.random() * charset.length));
     password += randomChar;
